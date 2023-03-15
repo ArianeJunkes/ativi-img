83ed3ebf4bf118c453eca350c734e207d0fc15d8
ArianeJunkes <arianejunkes312@gmail.com>
2023-03-15 11:34:55 -0300

    Atividade-Imagem

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Meus livros</Text>
+      <View style={styles.livros}>
+        <View style={styles.card}>
+          <Image style={styles.image} source={require('./assets/61acdiPRZQL.jpg')}/>
+          <Text style={styles.preco}>R$ 53,00</Text>
+        </View>
+        <View style={styles.card}>
+          <Image style={styles.image} source={require('./assets/Fundamentos-do-React.png')}/>
+          <Text style={styles.preco}>R$ 42,00</Text>
+        </View>
+
+      </View>
+      
     </View>
   );
 }
@@ -17,4 +28,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  livros: {
+    flexDirection: 'row'
+  },
+  card: {
+    width: 110,
+    height: 180,
+    backgroundColor: "black",
+    margin: 30,
+    alignItems: 'center',
+  },
+  image:{
+    height: 160,
+    width: 110,
+  },
+  preco:{
+    color: 'white',
+  }
 });
