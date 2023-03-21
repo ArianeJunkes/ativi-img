ccb8f934910c6eb20125f34ed4dc1c22b9c6d9bf
ArianeJunkes <arianejunkes312@gmail.com>
2023-03-21 16:51:41 -0300

    Atualizado

--- App.js
+++ App.js
@@ -1,48 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
+function Card(props) {
   return (
-    <View style={styles.container}>
-      <Text> Meus livros</Text>
-      <View style={styles.livros}>
-        <View style={styles.card}>
-          <Image style={styles.image} source={require('./assets/61acdiPRZQL.jpg')}/>
-          <Text style={styles.preco}>R$ 53,00</Text>
-        </View>
-        <View style={styles.card}>
-          <Image style={styles.image} source={require('./assets/Fundamentos-do-React.png')}/>
-          <Text style={styles.preco}>R$ 42,00</Text>
-        </View>
-
-      </View>
-      
+    <View style={styles.card}>
+      <Image style={styles.livro} source={{ uri: props.livro.capa }} />
+      <Text style={{ color: "darkred" }}> {props.livro.titulo} </Text>
+      <Text style={{ fontSize: 12 }}> R$ props.livro.preco</Text>
     </View>
   );
+}
+
+export default function App() {
+  const livros = [
+    {
+      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      titulo: 'Livro 1',
+      preco: 25.00,
+    },
+    {
+      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      titulo: 'Livro 1',
+      preco: 25.00,
+    },
+    {
+      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      titulo: 'Livro 2',
+      preco: 350.00,
+    },
+    {
+      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      titulo: 'Livro 3',
+      preco: 130.00,
+    },
+    {
+      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      titulo: 'Livro 4',
+      preco: 100.00,
+    },
+    {
+      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      titulo: 'Livro 5',
+      preco: 250.00,
+    },
+  ];
+  return (
+    <View>
+      <Text style={{fontSize: 25, fontWeigth: 'bold'}}>Olá</Text>
+      <View style={styles.conteudo}>
+        { livros.map(livro => <Card livro={livro} />)}
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0d0",
+    alignItems: "center",
+    paddingTop: 20,
+    justifyContent: "center",
   },
-  livros: {
-    flexDirection: 'row'
+  conteudo: {
+    flex: 1,
+    backgroundColor: "#ccc",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%',
   },
   card: {
-    width: 110,
-    height: 180,
-    backgroundColor: "black",
-    margin: 30,
-    alignItems: 'center',
-  },
-  image:{
-    height: 160,
-    width: 110,
-  },
-  preco:{
-    color: 'white',
+   backgroundColor: 'orange',
+   width: '40%',
+   height: 215,
+   borderRadius: 10,
+   margin: 15,
+    },
+  livro: {
+    resizeMode: 'stretch',
+    width: '100%',
+    height: 100,
   }
 });
