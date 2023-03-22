70614ae66365cab4460d3ceb6220f030293b7e7b
ArianeJunkes <arianejunkes312@gmail.com>
2023-03-22 10:56:54 -0300

    Finalizado!

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ function Card(props) {
   return (
     <View style={styles.card}>
       <Image style={styles.livro} source={{ uri: props.livro.capa }} />
-      <Text style={{ color: "darkred" }}> {props.livro.titulo} </Text>
-      <Text style={{ fontSize: 12 }}> R$ props.livro.preco</Text>
+      <Text style={{ color: "black" }}> {props.livro.titulo} </Text>
+      <Text style={{ fontSize: 12 }}> R$: {props.livro.preco}</Text>
     </View>
   );
 }
@@ -13,39 +13,41 @@ function Card(props) {
 export default function App() {
   const livros = [
     {
-      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      capa: "https://a-static.mlcdn.com.br/800x560/react-cookbook-packt-publishing/umlivro2/4919781783980727/f0ee70a81aec5f7aa936bfd8150f8086.jpeg",
       titulo: 'Livro 1',
       preco: 25.00,
     },
     {
-      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      capa: "https://a-static.mlcdn.com.br/800x560/react-cookbook-packt-publishing/umlivro2/4919781783980727/f0ee70a81aec5f7aa936bfd8150f8086.jpeg",
       titulo: 'Livro 1',
       preco: 25.00,
     },
     {
-      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      capa: "https://a-static.mlcdn.com.br/800x560/react-cookbook-packt-publishing/umlivro2/4919781783980727/f0ee70a81aec5f7aa936bfd8150f8086.jpeg",
       titulo: 'Livro 2',
       preco: 350.00,
     },
     {
-      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      capa: "https://a-static.mlcdn.com.br/800x560/react-cookbook-packt-publishing/umlivro2/4919781783980727/f0ee70a81aec5f7aa936bfd8150f8086.jpeg",
       titulo: 'Livro 3',
       preco: 130.00,
     },
     {
-      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      capa: "https://a-static.mlcdn.com.br/800x560/react-cookbook-packt-publishing/umlivro2/4919781783980727/f0ee70a81aec5f7aa936bfd8150f8086.jpeg",
       titulo: 'Livro 4',
       preco: 100.00,
     },
     {
-      capa: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.novatec.com.br%2Fcapas-ampliadas%2Fcapa-ampliada-9786586057393.jpg&tbnid=TV0at2hPMLEOFM&vet=12ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ..i&imgrefurl=https%3A%2F%2Fnovatec.com.br%2Flivros%2Freact-aprenda-praticando%2F&docid=S9tf-fEAHgT6JM&w=292&h=419&q=capa%20react&ved=2ahUKEwiw7pbL4-39AhVDNrkGHQ7jBxIQMygDegUIARDJAQ",
+      capa: "https://a-static.mlcdn.com.br/800x560/react-cookbook-packt-publishing/umlivro2/4919781783980727/f0ee70a81aec5f7aa936bfd8150f8086.jpeg",
       titulo: 'Livro 5',
       preco: 250.00,
     },
   ];
   return (
     <View>
-      <Text style={{fontSize: 25, fontWeigth: 'bold'}}>Olá</Text>
+      <View style={styles.texto}>
+      <Text >Olá Querido</Text>
+      </View>
       <View style={styles.conteudo}>
         { livros.map(livro => <Card livro={livro} />)}
       </View>
@@ -69,15 +71,23 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   card: {
-   backgroundColor: 'orange',
+   backgroundColor: 'gray',
    width: '40%',
-   height: 215,
+   height: 170,
    borderRadius: 10,
-   margin: 15,
+   margin: 20,
     },
   livro: {
     resizeMode: 'stretch',
     width: '100%',
-    height: 100,
-  }
+    height: 140,
+    borderRadius: 10,
+  },
+  texto: {
+    justifyContent: "center",
+    alignItems: 'center',
+    fontSize: 100, 
+    fontWeigth: 'bold',
+    paddingTop: "20%"
+  },
 });
